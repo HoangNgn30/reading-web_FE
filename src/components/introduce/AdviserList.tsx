d7a5433684657c4af80e81be0b5b6c1383b014dd
baooo04 <dbao09107@gmail.com>
2024-10-28 22:01:09 +0700

    update

--- src/components/introduce/AdviserList.tsx
+++ src/components/introduce/AdviserList.tsx
@@ -35,8 +35,7 @@ export default function AdviserList({ className }: IMemberListProps) {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        autoplay: false,
         responsive: [
             { breakpoint: 1024, settings: { slidesToShow: 2 } },
             { breakpoint: 640, settings: { slidesToShow: 1 } },
