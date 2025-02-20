56a535b0436e45a4fe2c6896d840c0c5b0c13264
xuandat7 <xuandattran2003@example.com>
2025-02-21 01:32:39 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import macrosPlugin from "vite-plugin-babel-macros"
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import macrosPlugin from "vite-plugin-babel-macros";
+
 export default defineConfig({
   plugins: [react(), macrosPlugin()],
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Chỉ có tác dụng khi chạy `vite dev`
   },
-})
+  build: {
+    outDir: 'dist', // Thư mục chứa file build
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Định nghĩa alias cho src
+    },
+  },
+});
